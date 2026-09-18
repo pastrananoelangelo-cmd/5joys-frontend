78181919b3ae901db6e002cd5a601c28de889400
pastrananoelangelo-cmd <pastrananoelangelo@gmail.com>
2026-09-18 09:48:27 +0800

    Fix Leaflet marker assets for production

--- src/pages/website/Locations.jsx
+++ src/pages/website/Locations.jsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { getLocations } from "../../services/website/locationService";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 import Section from "../../components/common/Section";
 import SectionHeading from "../../components/common/SectionHeading";
 
 import LocationCard from "../../components/website/LocationCard";
 import LocationsMap from "../../components/website/LocationsMap";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function LocationsPage() {
 
